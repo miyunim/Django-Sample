1e2c5c7a10c46ba3731ac7266fa4b6cd1c0fabd6
miyu <asukaily@gmail.com>
2012-08-27 02:26:46 +0900

    register page add

--- django_bookmarks/bookmarks/views.py
+++ django_bookmarks/bookmarks/views.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse, Http404
-from django.template import Context
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.template import Context, RequestContext
 from django.template.loader import get_template
 from django.contrib.auth.models import User
+from django.shortcuts import render_to_response
+from django.contrib.auth import logout
+from bookmarks.forms import *
 
 def main_page(request):
-	template = get_template('main_page.html')
-	variables = Context({
-		'head_title': '장고 북마크',
-		'page_title': '장고 북마크에 오신 것을 환영합니다.',
-		'page_body': '북마크를 저장하고 공유하세요!',
-	})
-	output = template.render(variables)
-	return HttpResponse(output)
+	return render_to_response(
+		'main_page.html',
+		RequestContext(request)
+	)
 
 def user_page(request, username):
 	try:
@@ -22,10 +21,30 @@ def user_page(request, username):
 	
 	bookmarks = user.bookmark_set.all()
 
-	template = get_template('user_page.html')
-	variables = Context({
+	variables = RequestContext(request, {
 		'username': username,
 		'bookmarks': bookmarks
 	})
-	output = template.render(variables)
-	return HttpResponse(output)
+	return render_to_response('user_page.html', variables)
+
+def logout_page(request):
+	logout(request)
+	return HttpResponseRedirect('/')
+
+def register_page(request):
+	if request.method == 'POST':
+		form = RegistrationForm(request.POST)
+		if form.is_valid():
+			user = User.objects.create_user(
+				username=form.cleaned_data['username'],
+				password=form.cleaned_data['password1'],
+				email=form.cleaned_data['email']
+			)
+			return HttpResponseRedirect('/')
+	else:
+		form = RegistrationForm()
+
+	variables = RequestContext(request, {
+		'form': form
+	})
+	return render_to_response('registration/register.html', variables)
